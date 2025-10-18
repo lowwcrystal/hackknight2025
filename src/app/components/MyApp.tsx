08371ae1df76b59cab1710f826c88ed705eb4ae3
ebicep <ebicep314@gmail.com>
2025-10-18 00:23:36 -0400

    fix: build error

--- src/app/components/MyApp.tsx
+++ src/app/components/MyApp.tsx
@@ -1,12 +1,11 @@
 "use client"; // required because it uses Radix client components
-import MyRadixApp from "../components/MyApp"; //adjust path as needed
-import { Flex, Text, Button } from "@radix-ui/themes";
+import {Button, Flex, Text} from "@radix-ui/themes";
 
 export default function MyApp() {
   return (
     <Flex direction="column" gap="2" className="mb-8">
       <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
+        <Button>Let&#39;s go</Button>
     </Flex>
   );
 }
