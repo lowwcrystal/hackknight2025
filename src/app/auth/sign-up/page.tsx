a299a291573683442d3529bbe9aa7f2bd8fd1d69
Munisa Ilhomova <milhomova1@gmail.com>
2025-10-18 01:43:48 -0400

    SignUpForm ui done

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -2,7 +2,8 @@ import SignUpForm from "@/app/components/SignUpForm";
 
 export default function Page() {
     return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="bg-gradient-to-b from-white via-orange-50 to-white
+ flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <SignUpForm/>
             </div>
