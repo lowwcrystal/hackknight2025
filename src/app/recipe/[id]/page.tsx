aa9320a19cbdae494515cc0d47d5fd1d628955c1
wwwwwwwwwwwwwmmmmmm <wwwwmmmm159262@gmail.com>
2025-10-19 11:42:05 -0400

    fix: stuff

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,7 +1,7 @@
 import {createClient} from "@/utils/supabase/server";
 import RecipeCard from "../../components/RecipeCard";
 import UserAttempts from "../../components/UserAttempts";
-import { Suspense } from "react";
+import {Suspense} from "react";
 
 interface RecipeRow {
     id: string;
@@ -41,26 +41,6 @@ export default async function RecipePage({params,}: {
             <Suspense fallback={<div className="p-4">Loading attempts...</div>}>
                 <UserAttempts recipeId={id} />
             </Suspense>
-            <div
-                key={id}
-                rel="noopener noreferrer"
-                className="overflow-hidden block"
-            >
-                <div className="p-4">
-                    <h4 className="text-5xl font-bold mb-5 bg-gradient-to-r from-orange-500 to-yellow-400 bg-clip-text text-transparent">
-                        Your Attempts
-                    </h4>
-                    <div className="grid grid-cols-1 md:grid-cols-8 gap-6">
-                        <a
-                            href={`${id}/evaluator`}
-                            className="bg-gradient-to-b from-yellow-100 to-orange-50 rounded-xl shadow-md border border-orange-200 p-0 overflow-hidden">
-                            <div className="bg-orange-500 text-white text-lg font-semibold py-25 text-center">
-                                +
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </section>
     );
 }
