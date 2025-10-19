1d4b2032bd254e76660daff8ba61e0d29320f463
wwwwwwwwwwwwwmmmmmm <wwwwmmmm159262@gmail.com>
2025-10-19 11:32:57 -0400

    feat: more background

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,7 +3,15 @@ import LoginForm from "@/app/components/LoginForm";
 export default function Page() {
     return (
         <div className="bg-gradient-to-b from-white via-orange-50 to-white
- flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+ flex min-h-svh w-full items-center justify-center p-6 md:p-10"
+             style={{
+                 backgroundImage: "url('/flames.jpg')",
+                 backgroundSize: "cover",
+                 backgroundRepeat: "no-repeat",
+                 backgroundPosition: "center",
+                 backgroundAttachment: "fixed"
+             }}
+        >
             <div className="w-full max-w-sm">
                 <LoginForm/>
             </div>
--- src/app/auth/update-password/page.tsx
+++ src/app/auth/update-password/page.tsx
@@ -2,7 +2,15 @@ import UpdatePasswordForm from "@/app/components/UpdatePasswordForm";
 
 export default function Page() {
     return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10"
+             style={{
+                 backgroundImage: "url('/flames.jpg')",
+                 backgroundSize: "cover",
+                 backgroundRepeat: "no-repeat",
+                 backgroundPosition: "center",
+                 backgroundAttachment: "fixed"
+             }}
+        >
             <div className="w-full max-w-sm">
                 <UpdatePasswordForm/>
             </div>
